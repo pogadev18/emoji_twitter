929f301032faa886f82cab49449a8cd71ebb8e4c
pogadev <pogadev18@gmail.com>
2023-03-28 18:52:34 +0300

    querying the posts

--- next.config.mjs
+++ next.config.mjs
@@ -18,5 +18,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.clerk.dev",
+      },
+    ],
+  },
 };
+
 export default config;

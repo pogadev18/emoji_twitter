767faf2a380176b05362d9cf645456b64a0d06fc
pogadev <pogadev18@gmail.com>
2023-04-07 14:05:43 +0300

    create posts procedure

--- src/components/CreatePostWizard.tsx
+++ src/components/CreatePostWizard.tsx
@@ -1,9 +1,18 @@
 import { useUser } from "@clerk/nextjs";
-
 import Image from "next/image";
+import { useState } from "react";
+
+import { api } from "~/utils/api";
 
 const CreatePostWizard = () => {
+  const [emoji, setEmoji] = useState("");
   const { user } = useUser();
+  const { mutate, isLoading: creatingPost } = api.posts.create.useMutation();
+
+  const createPost = () => {
+    mutate({ content: emoji });
+    setEmoji("");
+  };
 
   if (!user) return null;
 
@@ -17,10 +26,13 @@ const CreatePostWizard = () => {
         alt="profile image"
       />
       <input
+        value={emoji}
+        onChange={(e) => setEmoji(e.target.value)}
         type="text"
         placeholder="type some emojis"
         className="w-full bg-transparent outline-none"
       />
+      <button onClick={createPost}>Post</button>
     </div>
   );
 };
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,7 +1,12 @@
 import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import z from "zod";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -15,6 +20,9 @@ export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: {
+        createdAt: "desc",
+      },
     });
 
     const users = (
@@ -42,4 +50,17 @@ export const postRouter = createTRPCRouter({
       };
     });
   }),
+  create: privateProcedure
+    .input(z.object({ content: z.string().emoji().min(1).max(280) }))
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+      const { content } = input;
+
+      return await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content,
+        },
+      });
+    }),
 });
